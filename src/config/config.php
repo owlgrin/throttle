839490f64c84deaa39e4fd1e6ff6296b5aa00b61
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-01-14 12:54:38 +0530

    fixed the spaces

--- src/config/config.php
+++ src/config/config.php
@@ -42,20 +42,17 @@ return array(
 	/**
 	 * Set the default throttle plan.
 	 */
-	
 	'plan' => 'your-plan-identifier',
 	
 	/**
 	 * The default period class path to be used in throttle.
 	 */
-	
 	'period_class' => 'Owlgrin\Throttle\Period\CurrentMonthPeriod',
 
 	/**
 	 * Define feature indetifier and its seeder class path
 	 * which will be used while seeding daily base usage of features.
 	 */
-	
 	'seeders' => array(
 		'feature-identifier' => 'Seeder-class-path'
 	)
